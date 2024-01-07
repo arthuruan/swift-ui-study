d4628f67d392fb3489be4a4c559839557fb6d3d6
Arthur Ruan <arthur123ruan@hotmail.com>
2024-01-07 18:19:26 -0300

    Started Apple Framework App

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Arthur Ruan on 07/01/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }.padding()
+           
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+}
---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,52 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Arthur Ruan on 07/01/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                            .onTapGesture {
+                                print("")
+                            }
+                    }
+                }
+            }.navigationTitle("🍎 Frameworks")
+        }
+    }
+}
+
+#Preview {
+    FrameworkGridView().preferredColorScheme(.dark)
+}
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
